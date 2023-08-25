7cc3adbccdc89df1bbdfb51da2b31314b461fd49
Shreyas Chandrachoodan <shreyas.chandrachoodan@asl.mum.sst>
2023-08-25 16:08:46 +0530

    using error handling

--- Game.js
+++ Game.js
@@ -2,66 +2,78 @@ const Board = require("./Board")
 const Player = require("./Player")
 
 class Game {
-
+    #player1
+    #player2
+    #board
+    #isGameover
+    #turn
     constructor(player1, player2, board) {
-        this.player1 = player1
-        this.player2 = player2
-        this.board = board
-        this.isGameover = false
-        this.turn = 0
-
-
+        this.#player1 = player1
+        this.#player2 = player2
+        this.#board = board
+        this.#isGameover = false
+        this.#turn = 0
     }
     static Gamestart(player1Name, player2Name) {
-        let player1 = new Player(player1Name, 'X')
-        let player2 = new Player(player2Name, 'O')
-        let board = new Board()
-        if (player1Name == player2Name || typeof player1.name != 'string' || typeof player2.name != 'string') {
-            return ['Enter valid string', null]
+        try {
+            
+            let player1 = new Player(player1Name, 'X')
+            let player2 = new Player(player2Name, 'O')
+            let board = new Board()
+            if (player1Name == player2Name || typeof player1.name != 'string' || typeof player2.name != 'string') {
+                throw new Error('Enter valid string')
+            }
+            return ['object created', new Game(player1, player2, board)]
+        } catch (error) {
+            console.log(error.message);
         }
-
-        return ['object created', new Game(player1, player2, board)]
     }
 
     play(cellNumber) {
 
-        if (this.isGameover) {
-            return 'Game over'
+        try {
+            
+            if (this.#isGameover) {
+                return 'Game over'
+            }
+            if (typeof cellNumber != 'number' || cellNumber < 0 || cellNumber > 8) { //validation of checknumber
+    
+                throw new Error("The cellNumber is not present in the cell")
+            }
+            let isaCellEmpty = this.#board.isEmpty(cellNumber)
+    
+            if (!isaCellEmpty) {
+                return 'Cell is marked'
+            }
+    
+            let presentPlayer
+            if (this.#turn % 2 == 0) {
+                presentPlayer = this.#player1
+            }
+            else {
+                presentPlayer = this.#player2
+            }
+    
+            presentPlayer.markCell(this.#board.cells[cellNumber])
+    
+            this.#turn++;
+            this.#board.PrintBoard()
+    
+            if (this.#board.isWinner()) {
+                this.#isGameover = true
+                return `${presentPlayer.name} is the  winner`
+            }
+            if (this.#board.isDraw()) {
+                this.#isGameover = true
+                return "Its a Draw"
+            }
+            return 'Continue Playing'
+        } catch (error) {
+            console.log(error.message);
         }
-        if (typeof cellNumber != 'number' || cellNumber < 0 || cellNumber > 8) { //validation of checknumber
-
-            return 'The cellNumber is not present in the cell'
-        }
-        let isaCellEmpty = this.board.isEmpty(cellNumber)
-
-        if (!isaCellEmpty) {
-            return 'Cell is marked'
-        }
-
-        let presentPlayer
-        if (this.turn % 2 == 0) {
-            presentPlayer = this.player1
-        }
-        else {
-            presentPlayer = this.player2
-        }
-
-        presentPlayer.markCell(this.board.cells[cellNumber])
-
-        this.turn++;
-        this.board.PrintBoard()
-
-        if (this.board.isWinner()) {
-            this.isGameover = true
-            return `${presentPlayer.name} is the  winner`
-        }
-        if (this.board.isDraw()) {
-            this.isGameover = true
-            return "Its a Draw"
-        }
-        return 'Continue Playing'
 
     }
+
 }
 module.exports = Game
 
--- Index.js
+++ Index.js
@@ -1,11 +1,21 @@
 const Game = require("./Game");
 
 let [mgm,g1]= Game.Gamestart("Shreyas","Yash")
-console.log(g1)
+let[m,g2]=Game.Gamestart("Akash","Madura")
 
-console.log(g1.play(0))
-console.log(g1.play(2))
-console.log(g1.play(6))
-console.log(g1.play(8))
-console.log(g1.play(3))
+// console.log(g1.play(0)) //win 
+// console.log(g1.play(2))
+// console.log(g1.play(6))
+// console.log(g1.play(8))
+// console.log(g1.play(3))
 
+
+console.log(g2.play(0)); //draw
+console.log(g2.play(6));
+console.log(g2.play(4));
+console.log(g2.play(8));
+console.log(g2.play(7));
+console.log(g2.play(1));
+console.log(g2.play(2));
+console.log(g2.play(5));
+console.log(g2.play(3));
